dcbcd1f10c654b90a721be5f1d4bd742040a8072
AbdallatifTiyah <abdallatif.tiyah@gmail.com>
2026-07-24 18:08:04 +0300

    Lesson 68 API Update

--- app_one/controllers/property_api.py
+++ app_one/controllers/property_api.py
@@ -38,4 +38,30 @@ class PropertyApi(http.Controller):
             return request.make_json_response({
                 "message": "Property created successfully using JSON API"
             })
+
+
+    @http.route("/v1/property/<int:property_id>", methods=["PUT"], type="http", auth="none", csrf=False)
+    def update_property(self, property_id):
+        try:
+            property_record = request.env['property'].sudo().search([('id', '=', property_id)], limit=1)
+            if not property_record:
+                return request.make_json_response({
+                    "message": "Property not found"
+                }, status=404)
+    
+            args = request.httprequest.data.decode()
+            vals = json.loads(args)
+            property_record.sudo().write(vals)
+    
+            return request.make_json_response({
+                "id": property_record.id,
+                "name": property_record.name,
+                "description": property_record.description
+            })
         
+        except Exception as e:
+            return request.make_json_response({
+                "message": "Error updating property",
+                "error": str(e)
+            }, status=400)
+    
